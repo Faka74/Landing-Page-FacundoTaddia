3ed127ad20eeb495033bb4e8bdc2948da5be0d55
Facundo Taddia <facundotaddia35@gmail.com>
2023-08-08 15:53:49 -0300

    Final responsive added

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
-window.addEventListener('scroll', function() {
-    var navbar = document.querySelector('.navbar-fixed');
-  
-    if (window.scrollY > 0) {
-      navbar.classList.add('scrolled');
-      navbar.classList.remove('revert');
-    } else {
-      navbar.classList.remove('scrolled');
-      navbar.classList.add('revert');
-    }
-  });
+window.addEventListener('scroll', function () {
+  var navbar = document.querySelector('.navbar-fixed');
+
+  if (window.scrollY > 0) {
+    navbar.classList.add('scrolled');
+    navbar.classList.remove('revert');
+  } else {
+    navbar.classList.remove('scrolled');
+    navbar.classList.add('revert');
+  }
+});
+
+const barra = document.getElementById("barra")
+const ulDesplegable = document.getElementById("ulDesplegable")
+const uldesplegablequerie = document.getElementById("uldesplegablequerie")
+
+barra.addEventListener('click', () => {
+  uldesplegablequerie.classList.toggle('desplegado')
+})
+
+const barracierre = document.getElementById('barracierre')
+barracierre.addEventListener(("click"), () => {
+  uldesplegablequerie.classList.remove("desplegado");
+})
